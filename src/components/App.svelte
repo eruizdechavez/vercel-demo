af59d487bec204858285b250d908aed4eb2fd4d4
Erick Ruiz de Chavez <erick@ruizdechavez.com>
2020-06-06 11:20:17 -0400

    Add lorem ipsum

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -6,3 +6,16 @@
 
 <h1>Hello World!</h1>
 <Name {name} />
+<p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tristique,
+  enim in condimentum posuere, odio sem viverra elit, a feugiat purus quam vitae
+  urna. Morbi lacinia volutpat mi, id condimentum nulla consequat ut. Vestibulum
+  scelerisque, dui a molestie tempor, nibh erat laoreet ex, vitae venenatis nisl
+  massa ac purus. Pellentesque elementum lorem ut ligula rhoncus, id eleifend
+  metus molestie. Mauris lacinia nunc ipsum, eu eleifend risus sodales id.
+  Quisque varius eleifend elit, id scelerisque massa tempor at. Duis varius nibh
+  sapien, id rhoncus ex commodo at. Ut a ligula at sem posuere elementum in sed
+  nisi. Vestibulum volutpat justo at justo porta luctus. Ut quam ex, vulputate
+  sit amet metus in, tristique feugiat velit. Cras in velit eu mi imperdiet
+  commodo.
+</p>
